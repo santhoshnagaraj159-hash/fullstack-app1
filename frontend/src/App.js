036208371d9fc48a64d35bdd153d72410fbfb8a1
santhoshnagaraj159-hash <santhoshnagaraj159@gmail.com>
2026-06-10 12:03:26 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div>
+      <h1>DevOps Full Stack Project</h1>
+
+      <h2>Jenkins Master Agent</h2>
+
+      <h2>GitHub Webhook</h2>
+
+      <h2>Docker Deployment</h2>
+
+      <h2>Prometheus Monitoring</h2>
+
+      <h2>Grafana Dashboard</h2>
+    </div>
+  );
+}
+
+export default App;
